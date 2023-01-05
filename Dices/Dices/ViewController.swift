bbea7623927619da1846694efd981796a09b43df
@yanet26 <yanet-rodriguez@outlook.com.ar>
2023-01-04 21:47:39 -0300

    create project

---
+++ Dices/Dices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dices
+//
+//  Created by Yanet Rodriguez on 04/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
